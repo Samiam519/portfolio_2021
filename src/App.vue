635884813f31304693b28dd539c663292740c16b
Sam Jimenez <samanthajimenez@maximus.com>
2021-07-07 14:26:31 -0600

    resolve merge conflicts

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-=======
     <HelloWorld/>
->>>>>>> master
   </div>
 </template>
 
@@ -19,17 +14,3 @@ export default {
   }
 }
 </script>
-<<<<<<< HEAD
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
-=======
->>>>>>> master
