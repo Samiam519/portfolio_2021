cc66d261b1e3c2b4a03d9a4edb23dc436803c8ad
Sam Jimenez <samanthajimenez@maximus.com>
2021-07-08 15:44:33 -0600

    square layout

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -2,13 +2,9 @@
   <b-carousel :autoplay="false">
     <b-carousel-item v-for="(carousel, i) in carousels" :key="i">
       <section class="hero is-fullheight is-dark">
-        <div class="hero-body">
-          <div class="columns">
-            <div class="column is-half">
-              <h1 class="title is-1">Slide {{i+1}}</h1>
-              <p class="subtitle">{{carousel.content}}</p>
-            </div>
-          </div>
+        <div class="hero-body has-text-centered" id="slide-body">
+          <h1 class="title is-1">{{ carousel.name }}</h1>
+          <p class="subtitle">{{ carousel.content }}</p>
         </div>
       </section>
     </b-carousel-item>
@@ -18,16 +14,30 @@
 <script>
 export default {
   name: "Carousel",
-  data(){
+  data() {
     return {
       carousels: [
-        { text: 'Slide 1', color: 'primary', content: "this is content for slide 1" },
-        { text: 'Slide 2', color: 'info', content: "this is content for slide 2" },
-        { text: 'Slide 3', color: 'success', content: "this is content for slide 3" },
-        { text: 'Slide 4', color: 'warning', content: "this is content for slide 4" },
-        { text: 'Slide 5', color: 'danger', content: "this is content for slide 5" }
+        {name: 'Landing', content: "this is content for slide 1"},
+        {name: 'About Me', content: "this is content for slide 2"},
+        {name: 'Socials', content: "this is content for slide 3"},
+        {name: 'Contact', content: "this is content for slide 4"},
+        {name: 'Quidditch', content: "this is content for slide 5"},
+        {name: 'LunchLaunch', content: "this is content for slide 6"},
+        {name: 'Accomplish', content: "this is content for slide 7"},
+        {name: 'DSA', content: "this is content for slide 8"},
+        {name: 'Maximus', content: "this is content for slide 9"},
       ]
     }
   }
 }
 </script>
+
+<style scoped>
+#slide-body{
+  background-color: #181914;
+  border-radius: 50px;
+  margin: 2em auto;
+  width: 100vh;
+  max-width: 100vw;
+}
+</style>
