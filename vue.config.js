74cb663556ff799ae3ee7a2e3dc71211f4f59ff8
sam jimenez <samanthajimenez@maximus.com>
2021-09-28 15:51:25 -0600

    change path

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,8 @@
-module.exports = {
+/*module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
         ? '/portfolio_2021/'
         : '/'
-}
-/*
+}*/
 module.exports = {
     publicPath: '/'
-}*/
+}
